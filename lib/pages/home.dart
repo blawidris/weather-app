725593e3c193f13fceca99bb0a1f671371637355
blawidris <blawidris@gmail.com>
2023-02-27 05:49:54 +0100

    home page update

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,43 +10,61 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    data = ModalRoute.of(context)!.settings.arguments;
+    data = ModalRoute.of(context).settings.arguments;
+
+    //set background
+    String bgImage = data['isDaytime'] ? 'daytime.jpg' : 'night.jpg';
+    Color? bgColor = data['isDaytime'] ? Colors.blue : Colors.indigo[700];
+    Color? textColor = data['isDaytime'] ? Colors.grey[200] : Colors.white70;
 
     return Scaffold(
       body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            image:
+                DecorationImage(image: AssetImage(bgImage), fit: BoxFit.cover),
+          ),
           child: Padding(
-        padding: const EdgeInsets.fromLTRB(0.0, 120.0, 0, 0),
-        child: Column(
-          children: [
-            ElevatedButton.icon(
-              onPressed: () {
-                Navigator.pushNamed(context, '/location');
-              },
-              icon: Icon(Icons.edit_location),
-              label: Text('Edit Location'),
-            ),
-            SizedBox(height: 20.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            padding: const EdgeInsets.fromLTRB(0.0, 120.0, 0, 0),
+            child: Column(
               children: [
-                Text(
-                  data['location'],
-                  style: TextStyle(fontSize: 28.0, letterSpacing: 2.0),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/location');
+                  },
+                  icon: Icon(
+                    Icons.edit_location,
+                    color: textColor,
+                  ),
+                  label: Text(
+                    'Edit Location',
+                    style: TextStyle(color: textColor),
+                  ),
                 ),
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      data['location'],
+                      style: TextStyle(fontSize: 28.0, letterSpacing: 2.0),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  data['time'],
+                  style: TextStyle(
+                    fontSize: 66.0,
+                  ),
+                )
               ],
             ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              data['time'],
-              style: TextStyle(
-                fontSize: 66.0,
-              ),
-            )
-          ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -7,8 +7,13 @@ class WorldTime {
   String? time; // the time in that location
   String? flag; // uri for asset
   String? uri; // location url for api endpoint
+  bool? isDaytime; // true or false if dayytime or not
 
-  WorldTime({this.location, this.flag, this.uri});
+  WorldTime({
+    this.location,
+    this.flag,
+    this.uri,
+  });
 
   Future<void> getTime() async {
     try {
@@ -27,7 +32,9 @@ class WorldTime {
       DateTime now = DateTime.parse(dateTime);
       now = now.add(Duration(hours: int.parse(offset)));
 
-      time = DateFormat.jm().format(now); // set a time property
+      // set a time property
+      isDaytime = now.hour > 6 && now.hour < 20 ? true : false;
+      time = DateFormat.jm().format(now);
     } catch (e) {
       time = "could not get time data";
     }
